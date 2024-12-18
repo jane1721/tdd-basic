8713c3417bd585665f460b1bfacfd8cc69e3a4bc
jane <jane1721@gmail.com>
2024-12-19 00:22:27 +0900

    ✨Feat: Add TDD for retrieving user point history
    - 테스트 케이스 추가:
      - 정상적인 사용자 ID로 요청 시, 내역 리스트를 반환한다.
      - 존재하지 않는 사용자 ID로 요청 시, 빈 리스트를 반환한다.
      - 내역이 없는 사용자에 대해 빈 리스트를 반환한다.
    - 모든 테스트를 통과하도록 `PointService` 구현 완료
    
    Resolves: #2

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.exception.InvalidUserIdException;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     /**
      * 특정 유저의 포인트를 조회하는 기능
@@ -29,11 +31,16 @@ public class PointService {
         return userPointTable.selectById(userId);
     }
 
-    // TODO 특정 유저의 포인트 충전/이용 내역을 조회하는 기능
+    /**
+     * 특정 유저의 포인트 충전/이용 내역을 조회하는 기능
+     *
+     * @param userId    유저 ID
+     * @return          유저 포인트 충전/이용 내역 리스트
+     */
     List<PointHistory> history(
-            final long id
+            final long userId
     ) {
-        return List.of();
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 
     // TODO 특정 유저의 포인트를 충전하는 기능
