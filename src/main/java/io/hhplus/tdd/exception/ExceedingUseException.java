dd9ef8180c2ac9cbce95001485645b3393a29094
jane <jane1721@gmail.com>
2024-12-20 02:10:53 +0900

    🐛Fix: Corrected typo in error message
    - '최대 충전 금액을 초과하였습니다.' -> '사용 금액을 초과하였습니다.' (ExceedingUseException)

--- src/main/java/io/hhplus/tdd/exception/ExceedingUseException.java
+++ src/main/java/io/hhplus/tdd/exception/ExceedingUseException.java
@@ -3,7 +3,7 @@ package io.hhplus.tdd.exception;
 public class ExceedingUseException extends BaseCustomException {
 
     public ExceedingUseException() {
-        super("최대 충전 금액을 초과하였습니다.", "1004");
+        super("사용 금액을 초과하였습니다.", "1004");
     }
     public ExceedingUseException(String message) {
         super(message, "1004");
