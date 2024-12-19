4696d0269fd8ffcaa589002831bca06cf6aeed1a
jane <jane1721@gmail.com>
2024-12-19 22:33:23 +0900

    ✨Feat: Add TDD for charging user point
    - 테스트 케이스 추가:
      - 잘못된 형식(자연수가 아닐 때)의 사용자 ID 로 요청 시 Exception 이 발생한다.
      - 충전 금액이 0 또는 음수일 때, Exception 이 발생한다.
      - 충전 후 포인트가 10,000,000 를 초과할 경우, Exception 이 발생한다.
      - 그 외 케이스의 경우(충전 금액이 0 보다 크고, 충전된 포인트가 10,000,000 미만일 때), 해당 사용자의 충전 후 포인트 데이터를 반환한다.
    - 모든 테스트를 통과하도록 `PointService` 구현 완료
    
    Resolves: #3

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -2,6 +2,8 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.exception.ExceedingChargeException;
+import io.hhplus.tdd.exception.InvalidChargeAmountException;
 import io.hhplus.tdd.exception.InvalidUserIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -43,12 +45,38 @@ public class PointService {
         return pointHistoryTable.selectAllByUserId(userId);
     }
 
-    // TODO 특정 유저의 포인트를 충전하는 기능
+
+    /**
+     * 특정 유저의 포인트를 충전하는 기능
+     *
+     * @param userId    유저 ID
+     * @param amount    충전할 포인트
+     * @return          충전 후 유저 포인트
+     */
     UserPoint charge(
             final long userId,
             final long amount
     ) {
-        return new UserPoint(0, 0, 0);
+
+        if (userId <= 0) {
+            throw new InvalidUserIdException();
+        }
+
+        if (amount <= 0) {
+            throw new InvalidChargeAmountException();
+        }
+
+        UserPoint originalUserPoint = userPointTable.selectById(userId);
+
+        if (originalUserPoint.point() + amount > 10_000_000) {
+            throw new ExceedingChargeException();
+        }
+
+        long updatedPoint = originalUserPoint.point() + amount;
+
+        pointHistoryTable.insert(userId, updatedPoint, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return userPointTable.insertOrUpdate(userId, updatedPoint);
     }
 
     // TODO 특정 유저의 포인트를 사용하는 기능
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -2,6 +2,8 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.exception.ExceedingChargeException;
+import io.hhplus.tdd.exception.InvalidChargeAmountException;
 import io.hhplus.tdd.exception.InvalidUserIdException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -140,6 +142,97 @@ public class PointServiceTest {
 
             // then
             assertThat(actualHistories).isEmpty();
+        }
+    }
+
+    @Nested
+    @DisplayName("charge - 특정 유저의 포인트를 충전하는 기능")
+    class ChargeTest {
+
+        /**
+         * 3. 특정 유저의 포인트를 충전하는 기능
+         *  - 잘못된 형식(자연수가 아닐 때)의 사용자 ID 로 요청 시 Exception 이 발생한다.
+         *  - 충전 금액이 0 또는 음수일 때, Exception 이 발생한다.
+         *  - 충전 금액이 1,000 미만일 때, Exception 이 발생한다.
+         *  - 충전 후 포인트가 10,000,000 를 초과할 경우, Exception 이 발생한다.
+         *  - 충전 내역을 저장하지 못 할 경우, Exception 이 발생한다.
+         *  - 그 외 케이스의 경우(충전 금액이 0 보다 크고, 충전된 포인트가 10,000,000 미만일 때), 해당 사용자의 충전 후 포인트 데이터를 반환한다.
+         */
+
+        @DisplayName("잘못된 형식(자연수가 아닐 때)의 사용자 ID 로 요청 시 Exception 이 발생한다.")
+        @Test
+        void chargeNonUserPointFail() throws Exception {
+            // given
+            final long negativeUserId = -1L;
+            final long zeroUserId = 0;
+
+            // when
+            // then
+            assertThrows(
+                    InvalidUserIdException.class,
+                    () -> pointService.charge(negativeUserId, 1_000L)
+            );
+
+            assertThrows(
+                    InvalidUserIdException.class,
+                    () -> pointService.charge(zeroUserId, 1_000L)
+            );
+        }
+
+        @DisplayName("충전 금액이 0 또는 음수일 때, Exception 이 발생한다.")
+        @Test
+        void chargeInvalidAmountFail() throws Exception {
+            // given
+            final long userId = 1L;
+            final long negativeAmount = -1_000L;
+            final long zeroAmount = 0L;
+
+            // when
+            // then
+            assertThrows(
+                    InvalidChargeAmountException.class,
+                    () -> pointService.charge(userId, negativeAmount)
+            );
+
+            assertThrows(
+                    InvalidChargeAmountException.class,
+                    () -> pointService.charge(userId, zeroAmount)
+            );
+        }
+
+        @DisplayName("충전 후 포인트가 10,000,000 를 초과할 경우, Exception 이 발생한다.")
+        @Test
+        void chargeExceedingPointFail() throws Exception {
+            // given
+            final long userId = 1L;
+            final long originalUserPoint = 9_999_999L;
+            final long chargeAmount = 2L;
+            given(userPointTable.selectById(userId)).willReturn(new UserPoint(userId, originalUserPoint + chargeAmount, System.currentTimeMillis()));
+
+            // when
+            // then
+            assertThrows(
+                    ExceedingChargeException.class,
+                    () -> pointService.charge(userId, chargeAmount)
+            );
+        }
+
+        @DisplayName("그 외 케이스의 경우(충전 금액이 0 보다 크고, 충전된 포인트가 10,000,000 미만일 때), 해당 사용자의 충전 후 포인트 데이터를 반환한다.")
+        @Test
+        void chargeValidAmountSuccess() throws Exception {
+            // given
+            final long userId = 1L;
+            final long originalAmount = 1_000L;
+            final long chargeAmount = 5_000L;
+            UserPoint expectedUserPoint = new UserPoint(userId, originalAmount + chargeAmount, System.currentTimeMillis());
+            given(userPointTable.selectById(userId)).willReturn(new UserPoint(userId, originalAmount, System.currentTimeMillis()));
+            given(userPointTable.insertOrUpdate(userId, originalAmount + chargeAmount)).willReturn(expectedUserPoint);
+
+            // when
+            UserPoint actualUserPoint = pointService.charge(userId, chargeAmount);
+
+            // then
+            assertThat(actualUserPoint).isEqualTo(expectedUserPoint);
         }
     }
 }
